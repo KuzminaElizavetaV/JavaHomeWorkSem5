241bb18b57a15b3811f435fe96be1bbf69f1b4b2
Elizaveta <hamlizuha@mail.ru>
2023-02-07 00:55:59 +0300

    дабавила комментарии в код задачи 3 (сортировка кучей HeapSort)

--- Task3.java
+++ Task3.java
@@ -13,23 +13,23 @@ public class Task3 {
         int size = array.length;
         for (int i = size / 2 - 1; i >=0 ; i--)
             buildHeap(array, i, size);
-        for (int i = size - 1; i >= 0 ; i--) {
-            int temp = array[i];
+        for (int i = size - 1; i >= 0 ; i--) {// Фаза сортировки, многократно извлекает максимум и наводит порядок в куче
+            int temp = array[i]; // Переместить текущий корень в конец
             array[i] = array[0];
             array[0] = temp;
-            buildHeap(array, 0, i);
+            buildHeap(array, 0, i); //корневой элемент в правильное положение на уменьшенной куче
         }
     }
     private static void buildHeap(int[] array, int i, int size) {
         int leftChild = i * 2 + 1;
         int rightChild = i * 2 + 2;
         int maxChild = i;
-
+        // найти наибольший среди корневых, левых и правых "детей"
         if(leftChild < size && array[leftChild] > array[maxChild])
             maxChild = leftChild;
         if(rightChild < size &&  array[rightChild] > array[maxChild])
             maxChild = rightChild;
-        if(i != maxChild){
+        if(i != maxChild){ //если наибольший не корневой - менять местами и продолжить
             int temp = array[i];
             array[i] = array[maxChild];
             array[maxChild] = temp;
