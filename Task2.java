28a94f54f360cb7986255494fc8098f00b5d0373
Elizaveta <hamlizuha@mail.ru>
2023-02-06 22:40:26 +0300

    в задаче 2 реализован метод сортировки словаря по значению

--- Task2.java
+++ Task2.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
+import java.util.stream.Collectors;
 /**Пусть дан список сотрудников: Иван, Пётр, Антон и так далее. Написать программу, которая найдет и выведет
  * повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
  */
@@ -12,18 +9,33 @@ public class Task2 {
                 "Елизавета", "Дорофей", "Николай", "Ирина", "Иван", "Дорофей", "Татьяна", "Евгений", "Анатолий",
                 "Светлана", "Николай", "Иван", "Наталия", "София", "Кирилл", "Ярослав", "Николай", "Петр", "Владимир",
                 "Александр", "София", "Елизавета", "Людмила", "Евгений", "Иван", "Ирина", "Вячеслав", "Ярослав"));
-        countDuplicateNames(listEmployees);
+        sortValuesDescendingOrder(countDuplicateNames(listEmployees));
     }
-    private static void countDuplicateNames(ArrayList<String> listNames){ //считать повтояющиеся имена
+    private static Map<String, Integer> countDuplicateNames(ArrayList<String> listNames) { //считать повтояющиеся имена
         Map<String, Integer> nameQuantity = new HashMap<>();
         for (String name : listNames) {
-            if(nameQuantity.containsKey(name)) {
+            if (nameQuantity.containsKey(name)) {
                 int count = nameQuantity.get(name);
                 nameQuantity.put(name, ++count);
             } else nameQuantity.put(name, 1);
         }
-        for (Map.Entry<String, Integer> entry: nameQuantity.entrySet()) {
+        System.out.println("РЕЗУЛЬТАТ ПОДСЧЕТА ПОВТОРЯЮЩИХСЯ ИМЕН:");
+        for (Map.Entry<String, Integer> entry : nameQuantity.entrySet()) {
             System.out.printf("%-10s %-5s\n", entry.getKey(), entry.getValue());
+        }
+        return nameQuantity;
+    }
+    private static void sortValuesDescendingOrder (Map<String, Integer> unsortedMap) {
+        //сортировка по значению с помощью LinkedHashMap, нашла вот такое решение, на мой взгляд, самое оптимальное
+        Map<String, Integer> sortedMap = unsortedMap.entrySet().stream().
+                sorted(Comparator.comparingInt(e -> -e.getValue())).collect(Collectors.
+                        toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> {throw new AssertionError();},
+                                LinkedHashMap::new));
+        System.out.println();
+        System.out.println("ОТСОРТИРОВАННЫЙ СПИСОК ПО УБЫВАНИЮ ПОПУЛЯРНОСТИ:");
+        for (Map.Entry<String, Integer> elem : sortedMap.entrySet()) {
+            System.out.printf("%-10s %-5s\n", elem.getKey(), elem.getValue());
         }
     }
 }
+
